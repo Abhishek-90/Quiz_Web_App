be582b5587b45635883f7755e3478e1d852e0307
Abhishek <holaniabhishek90@gmail.com>
2020-12-08 14:01:58 +0530

    Temp Commit

--- New Exam.js
+++ New Exam.js
@@ -4,6 +4,26 @@ function addQuestion(){
     ques.setAttribute("placeholder","Enter Question.....");
     ques.setAttribute("class","questions");
 
-    var ques_div = document.getElementById("Question");
-    ques_div.append(ques);
+    var option_1 = document.createElement("input");
+    option_1.setAttribute("type","text");
+    option_1.setAttribute("class","answer");
+
+    var option_2 = document.createElement("input");
+    option_2.setAttribute("type","text");
+    option_2.setAttribute("class","answer");
+
+    var option_3 = document.createElement("input");
+    option_3.setAttribute("type","text");
+    option_3.setAttribute("class","answer");
+    
+    var option_4 = document.createElement("input");
+    option_4.setAttribute("type","text");
+    option_4.setAttribute("class","answer");
+
+    var ques_ans_div = document.getElementById("Question-Answer");
+    ques_ans_div.append(ques);
+    ques_ans_div.append(option_1);
+    ques_ans_div.append(option_2);
+    ques_ans_div.append(option_3);
+    ques_ans_div.append(option_4);
 }
